d4ea0c5ce048caa6c935facf33b454441c7c0f92
kent <kent.26pg@gmail.com>
2026-05-05 14:35:14 +0200

    Task 1 Done

---
+++ script.js
@@ -0,0 +1,13 @@
+function checkOddOrEven(num) {
+    if (num % 2 === 0) {
+        return "Even";
+    } else {
+        return "Odd"
+    }
+}
+
+console.log(checkOddOrEven(2));
+console.log(checkOddOrEven(5));
+console.log(checkOddOrEven(10));
+console.log(checkOddOrEven(17));
+console.log(checkOddOrEven(18));
