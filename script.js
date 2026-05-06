171fb9ca1e88879b166803be9ebf1a7093c23396
kent <kent.26pg@gmail.com>
2026-05-06 14:51:02 +0200

    finished for today

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //  Oppgave 1
 const checkOddOrEven = (num) => {
-    if (num % 2 === 0) {
-        return "Even";
-    } else {
-        return "Odd"
-    }
-}
+  if (num % 2 === 0) {
+    return "Even";
+  } else {
+    return "Odd";
+  }
+};
 
 console.log(checkOddOrEven(1));
 console.log(checkOddOrEven(5));
@@ -16,10 +16,10 @@ console.log(checkOddOrEven(18));
 // Oppgave 2
 
 function exString1(text) {
-    return text.toUpperCase() + "!"; 
-} 
-console.log(exString1("hello"))
-console.log(exString1("hope you are doing fine"))
+  return text.toUpperCase() + "!";
+}
+console.log(exString1("hello"));
+console.log(exString1("hope you are doing fine"));
 
 const exString2 = (text) => text.toUpperCase() + "!";
 console.log(exString2("this is a test for arrow function"));
@@ -27,19 +27,24 @@ console.log(exString2("this is a test for arrow function"));
 // oppgave 3
 
 const hils = (navn, time) => {
-    if (time === undefined  || time === null) {
+  if (time === undefined || time === null) {
     return "Feil ingen time verdi avgitt";
-     }
-     if (time < 0 || time > 23 ) {
-        return "ugyldig tid"
-         }
-        if (time <= 5)  return `God natt, ${navn}`;
-         if (time <= 11) return `God morgen, ${navn}`;
-         if (time <= 17) return `God dag, ${navn}`;
-         if (time <= 23) return `God kveld, ${navn}`;
-     };
- 
-     console.log(hils("Kurt", 18));
+  }
+  if (time < 0 || time > 23) {
+    return "ugyldig tid";
+  }
+  if (time <= 5) return `God natt, ${navn}`;
+  if (time <= 11) return `God morgen, ${navn}`;
+  if (time <= 17) return `God dag, ${navn}`;
+  if (time <= 23) return `God kveld, ${navn}`;
+};
 
-    //  oppgave 4
-    
+console.log(hils("Kurt", 18));
+
+//  oppgave 4
+
+function removeArrays(arr) {
+  return arr.slice(0, -1);
+}
+
+console.log(removeArrays(["Rød", "Grønn", "Blå", "Gul"]));
