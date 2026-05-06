3c7ba1d59c9379c77ac7e0dbf67eec8997374282
kent <kent.26pg@gmail.com>
2026-05-06 13:39:45 +0200

    have begun on task 2

--- script.js
+++ script.js
@@ -12,3 +12,10 @@ console.log(checkOddOrEven(5));
 console.log(checkOddOrEven(10));
 console.log(checkOddOrEven(17));
 console.log(checkOddOrEven(18));
+
+// Oppgave 2
+
+function gjørStoreBokstaver(text) {
+    return text.toUpperCase() + "!"; 
+} 
+console.log(gjørStoreBokstaver("hallo"))
